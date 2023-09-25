46d3b6846524a6b12a39841edf755470b3014fa4
JavaCommons Technologies <javacommons@gmail.com>
2023-09-25 22:36:41 +0900

    .

--- oml/tests/01_test.mjs
+++ oml/tests/01_test.mjs
@@ -108,7 +108,7 @@ Deno.test("01_test #10", () => {
 });
 
 Deno.test("01_test #11", () => {
-    let result = run(`
+    let result = exec(`
         @person.hobbies[1]@
         `);
     result = ast2oml(result);
@@ -116,7 +116,7 @@ Deno.test("01_test #11", () => {
 });
 
 Deno.test("01_test #12", () => {
-    let result = run(`
+    let result = exec(`
         (set! @person.hobbies[1]@ "golf"}
         @person.hobbies[1]@
         `);
@@ -125,17 +125,17 @@ Deno.test("01_test #12", () => {
 });
 
 Deno.test("01_test #13", () => {
-    let result = run("`abc``def`");
+    let result = exec("`abc``def`");
     asst.assertEquals(result, "abc`def");
 });
 
 Deno.test("01_test #14", () => {
-    let result = run("(define a `abc`)(define b `xyz`)`${a}-${b}`");
+    let result = exec("(define a `abc`)(define b `xyz`)`${a}-${b}`");
     asst.assertEquals(result, "abc-xyz");
 });
 
 Deno.test("01_test #15", () => {
-    let result = run(
+    let result = exec(
         "(do [(x 1 (+ x 1))" + "\n" +
         "     (result 0]" + "\n" +
         "    [(> x 3) result]" + "\n" +
@@ -148,7 +148,7 @@ Deno.test("01_test #15", () => {
 });
 
 Deno.test("01_test #16", () => {
-    let result = run(
+    let result = exec(
         "(do* [(x 1 (+ x 1))" + "\n" +
         "      (result 0]" + "\n" +
         "     [(> x 3) result]" + "\n" +
@@ -161,7 +161,7 @@ Deno.test("01_test #16", () => {
 });
 
 Deno.test("01_test #17", () => {
-    let result = run(`
+    let result = exec(`
         (define x 10)
         (define y 20)
         (do
@@ -175,7 +175,7 @@ Deno.test("01_test #17", () => {
 });
 
 Deno.test("01_test #18", () => {
-    let result = run(`
+    let result = exec(`
         (define x 10)
         (define y 20)
         (do*
